2c6123fc119abf7a946e365f35f5a4630ecd02bb
AfsanAbdul <afsanabdullayeva@mai.ru>
2018-10-02 23:53:22 +0400

    Project

---
+++ js/main.js
@@ -0,0 +1,79 @@
+var btn = $('.buttn .tb1');
+btn.click(function () {
+    $('.tb2').toggleClass('active');
+    $('.tb3').toggleClass('active2');
+
+    var subscribe = $('#forpart .buttn .subscribe')
+    if ($('body').width() < 767) {
+        subscribe.toggleClass('subactive')
+    }
+})
+
+// hover Slide
+
+$(function () {
+    var arrImg = ["img/Layer 18.png", "img/mac-png-7.png", "img/ipad.png", "img/iphone-x.png", "img/ipod.png"];
+    var slide = $("#twopart .bxslider img");
+    var slideLi = $("#twopart .list-inline li");
+
+    slideLi.mouseover(function (e) {
+        slideLi.removeClass("active");
+        $(this).addClass("active");
+        val = e.target.getAttribute("data-value");
+        slide.attr("src", arrImg[val]);
+
+    })
+})
+
+/*header slide*/
+$(function () {
+    var arrimage = ["img/Layer 18.png", "img/mac-png-7.png", "img/ipad.png"];
+    var arrtext = ["iPhone-X", "iMac", "iPod"];
+    var arrpar = ["Say hello to the future", "With Retina 5k Display", "Say hello to the future"];
+
+    var phoneImg = $(" section .two img ");
+    var phoneH1 = $("header .first h1");
+    var phoneP = $(" header .first p");
+
+    var i = 1;
+
+    function slide() {
+        phoneImg.attr('src', arrimage[i])
+        phoneH1.html(arrtext[i])
+        phoneP.html(arrpar[i])
+
+        i++;
+        if (i == arrtext.length) {
+            i = 0;
+        }
+    }
+
+    setInterval(slide, 5000);
+})
+
+$('.triangle-move').click(function () {
+    $('.triangle').animate({
+        'top': '125px'
+    },500);
+});
+$('.clss1').click(function () {
+    $('.triangle').animate({
+        'top': '45px'
+    },500);
+});
+$('.clss2').click(function () {
+    $('.triangle').animate({
+        'top': '195px'
+    },500);
+});
+
+// MAP START
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.3774315, lng: 49.8541026},
+        scrollwheel: false,
+        zoom: 17
+    });
+}
+
+// MAP END
